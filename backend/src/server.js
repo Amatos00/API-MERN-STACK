7971faed43bea53822a5e768deae0ef0e9ffedc8
Anderson de Oliveira <amatos.oliveira2197@gmail.com>
2020-12-29 21:29:59 -0300

    connecting to mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
-app.use(express.json());
-
-app.post('/users', (req, res) => {
-    return res.json(req.body);
+mongoose.connect('mongodb+srv://amatos:amatos@cluster0.x8fas.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
+app.use(express.json());
+app.use(routes);
+
 app.listen(3333);
+
+
